3931f45fa5af0e587ace7a6ace9a2ecd96a6caaa
Ricardo <ricardo.sergio.pff@gmail.com>
2024-11-11 10:17:43 -0300

    Adição das interações com metas

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-from utils.funcoes import mostrar_menu, pergunta_usuario, perguntar_valores_adicionar, perguntar_valores_atualizar
+from utils.funcoes import *
 from controllers.treino_controller import adicionar_treino, ler_treinos, atualizar_treino
+from controllers.metas_controller import adicionar_meta, atualizar_meta, mostrar_metas
 
 def main():
     mostrar_menu()
@@ -17,7 +18,23 @@ def main():
         atualizar_treino(nome_treino, novos_dados)
 
     elif resposta_usuario == 4:
-        print("Template")
+        mostrar_menu_metas()
+
+        resposta_usuario_metas = pergunta_usuario()
+
+        if resposta_usuario_metas == 1:
+            dados_meta = perguntar_valores_meta_adicionar()
+            adicionar_meta(**dados_meta)
+
+        elif resposta_usuario_metas == 2:
+            mostrar_metas()
+
+        elif resposta_usuario_metas == 3:
+            nome_meta, novos_dados_meta = perguntar_valores_meta_atualizar()
+            atualizar_meta(nome_meta, novos_dados_meta)
+
+        elif resposta_usuario_metas == 4:
+            print("Template")
 
     return True
 
