3a7a45a061e2019670c797d018a11918ad29f49e
Ricardo <ricardo.sergio.pff@gmail.com>
2024-11-11 10:49:42 -0300

    Alteração na Main

--- src/main.py
+++ src/main.py
@@ -4,39 +4,53 @@ from controllers.treino_controller import adicionar_treino, ler_treinos, atualiz
 from controllers.metas_controller import adicionar_meta, atualizar_meta, mostrar_metas
 
 def main():
+    # Clear Console
     os.system('cls' if os.name == 'nt' else 'clear')
     mostrar_menu()
     resposta_usuario = pergunta_usuario()
 
+    # Adicionar Treinos
     if resposta_usuario == 1:
         dados_treino = perguntar_valores_adicionar()
         adicionar_treino(**dados_treino)
 
+    # Ler Treinos
     elif resposta_usuario == 2:
         ler_treinos()
 
+    # Atualizar Treinos
     elif resposta_usuario == 3:
         nome_treino, novos_dados = perguntar_valores_atualizar()
         atualizar_treino(nome_treino, novos_dados)
 
+    # Deletar Treinos
     elif resposta_usuario == 4:
+        print("Template")
+
+    # Menu Metas
+    elif resposta_usuario == 5:
+        #Clear Console
         os.system('cls' if os.name == 'nt' else 'clear')
 
         mostrar_menu_metas()
 
         resposta_usuario_metas = pergunta_usuario()
 
+        # Adicionar Metas
         if resposta_usuario_metas == 1:
             dados_meta = perguntar_valores_meta_adicionar()
             adicionar_meta(**dados_meta)
 
+        # Ler Metas
         elif resposta_usuario_metas == 2:
             mostrar_metas()
 
+        # Atualizar Metas
         elif resposta_usuario_metas == 3:
             nome_meta, novos_dados_meta = perguntar_valores_meta_atualizar()
             atualizar_meta(nome_meta, novos_dados_meta)
 
+        # Deletar Metas
         elif resposta_usuario_metas == 4:
             print("Template")
 
